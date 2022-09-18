8b6d79124f2f49fac8832335faba8d9b962f9774
Tarasov Ivan <mr.wilfor@gmail.com>
2022-09-18 13:43:26 +0300

    initial commit

---
+++ app/src/main/java/com/example/android/compas/CompassActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.compas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compass)
+    }
+}
